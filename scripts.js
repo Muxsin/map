47bed98982c352791198e2f905e1eb8913268822
Furayd1n <aminovfuraidin@gmail.com>
2024-05-10 19:09:06 +0700

    Created function for drawing a line

--- scripts.js
+++ scripts.js
@@ -3,3 +3,12 @@ const context = canvas.getContext("2d");
 
 canvas.width= 500;
 canvas.height = 500;
+
+function resiz(x1, y1, x2, y2) {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
+
+resiz(100, 100, 300, 300);
