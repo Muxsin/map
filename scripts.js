99c94e706e687651fb98589858b5d63d7e095f39
Furayd1n <aminovfuraidin@gmail.com>
2024-05-12 04:01:39 +0700

    Resolved requested changes

--- scripts.js
+++ scripts.js
@@ -4,11 +4,9 @@ const context = canvas.getContext("2d");
 canvas.width= 500;
 canvas.height = 500;
 
-function resiz(x1, y1, x2, y2) {
+function drawLine(x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
 }
-
-resiz(100, 100, 300, 300);
