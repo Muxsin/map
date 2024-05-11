3bcdcac5200971c58223e4e76fd76617ce37c002
Cosmos <152709131+cosmos020805@users.noreply.github.com>
2024-05-12 04:20:35 +0700

    Styled canvas. Added function to draw lines. Added function to handle clicked coordinates (#2)

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-canvas.width= 500;
-canvas.height = 500;
+canvas.width= 1850;
+canvas.height = 825;
+
+function handleClick(e) {
+    const x = e.clientX - canvas.offsetLeft;
+    const y = e.clientY - canvas.offsetTop;
+
+    console.log(`Clicked at: (${x.toFixed(2)}, ${y.toFixed(2)})`);
+}
+
+canvas.addEventListener('click', handleClick);
 
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+}
+
+function drawLinesFromObject(coordObject) {
+    for (const key in coordObject) {
+        if (coordObject.hasOwnProperty(key)) {
+            const lines = coordObject[key];
+            lines.forEach(line => {
+                drawLine(line.x1, line.y1, line.x2, line.y2);
+            });
+        }
+    }
 }
