8b0628f5067bc25acbdaeef06204477a717736af
Aminov Furaydin <132105423+Furayd1n@users.noreply.github.com>
2024-05-12 04:03:19 +0700

    Created function for drawing a line (#1)

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ const context = canvas.getContext("2d");
 
 canvas.width= 500;
 canvas.height = 500;
+
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
